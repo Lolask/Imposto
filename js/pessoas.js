50d699028e02179074197445db718f42fbe95aa7
lola <sakamoryloh37@gmail.com>
2025-03-07 22:25:45 -0300

    commit v2

---
+++ js/pessoas.js
@@ -0,0 +1,31 @@
+"use strict";
+var empresa;
+(function (empresa) {
+    class Pessoa {
+        get produto() {
+            return this._produto;
+        }
+        set produto(produto) {
+            this._produto = produto;
+        }
+        get preco() {
+            return this._preco;
+        }
+        set preco(preco) {
+            this._preco = preco;
+        }
+        get imposto() {
+            return this.imposto;
+        }
+        set imposto(imposto) {
+            this._imposto = imposto;
+        }
+        calcularImposto() {
+            return (this._imposto * this._preco) / 100;
+        }
+        calcularImposto2() {
+            return (this._imposto * this._preco) / 100 + this._preco;
+        }
+    }
+    empresa.Pessoa = Pessoa;
+})(empresa || (empresa = {}));
---
+++ ts/pessoas.ts
@@ -0,0 +1,39 @@
+namespace empresa {
+    export class Pessoa {
+          private _produto: string;
+          private _preco: number;
+          private _imposto: number;
+  
+          get produto(): string {
+          return this._produto;
+          }
+  
+          set produto(produto: string) {
+          this._produto = produto;
+          }
+  
+          get preco(): number {
+          return this._preco;
+          }
+  
+          set preco(preco: number) {
+          this._preco = preco;
+          }
+
+          get imposto(): number {
+            return this.imposto;
+          }
+    
+          set imposto(imposto: number) {
+            this._imposto = imposto;
+          }
+  
+          public calcularImposto(): number {
+          return (this._imposto * this._preco) / 100;
+          }
+  
+          public calcularImposto2(): number {
+              return (this._imposto * this._preco) / 100 + this._preco;
+          }
+      }
+  }
